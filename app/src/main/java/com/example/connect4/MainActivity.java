4279545b29e518365afdee292d8ad0a05e1b4ad4
shilo <dadon5609@gmail.com>
2023-09-18 19:37:06 +0300

    d

--- app/src/main/java/com/example/connect4/MainActivity.java
+++ app/src/main/java/com/example/connect4/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.connect4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener((view) -> {
+
+            ImageView imageView = (ImageView) findViewById(R.id.button43);
+            imageView.setBackgroundResource(R.drawable.peace);
+
+        });
     }
 }
